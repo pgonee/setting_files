482e47df04fed960193737e50d2cf237206e7c46
pgonee <pgonee@pg-macbook.(none)>
2013-01-16 01:37:53 +0900

    sfsadf

--- mac/fanctl.py
+++ mac/fanctl.py
@@ -11,6 +11,10 @@ def get_max_rpm():
     rst = commands.getoutput('cat /sys/devices/platform/applesmc.768/fan1_max')
     return int(rst)
 
+def get_rpm():
+    a, b = commands.getstatusoutput('cat /sys/devices/platform/applesmc.768/fan1_min')
+    return int(b)
+
 def set_rpm(rpm_speed):
     a, b = commands.getstatusoutput('echo %d > /sys/devices/platform/applesmc.768/fan1_min' % (rpm_speed))
 
@@ -18,9 +22,12 @@ def main():
     min_temp = 30
     max_temp = 80
     min_rpm = 1500
-    max_rpm = get_max_rpm()
+    max_rpm = get_max_rpm() - 201
     current_temp = get_temp() / 1000
     #print "current : %d" % (current_temp)
+
+    if get_rpm() > max_rpm:
+        return
 
     if current_temp <= min_temp:
         set_rpm(min_rpm)
